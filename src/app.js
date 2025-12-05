5fc704dc24ca5f81973bb2c8f010b9467f7a022c
sinyeongseok <sinyeongseok99@gmail.com>
2025-12-05 00:32:05 +0000

    feat: CORS 패키지 설치 및 전역 CORS 설정

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes/index.js";
+import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 app.use(errorHandler);
 
